34773181d063a8f90f53ebb3424bffac6d655b90
alyssa rodriguez <amrrodriguez@ucdavis.edu>
2023-01-12 22:56:25 -0800

    muted attribute fixed

--- src/components/video-wrapper/VideoWrapper.js
+++ src/components/video-wrapper/VideoWrapper.js
@@ -1,10 +1,19 @@
 import styles from './VideoWrapper.module.css';
 import Video from '../../assets/video/black.mp4';
+import { useRef, useEffect } from 'react';
 
 const VideoWrapper = (props) => {
+    const videoRef = useRef(null);
+
+    // this fixed react ignoring muted attribute 
+    // need it to be muted for autoPlay
+    useEffect(() => {
+        videoRef.current.defaultMuted = true;
+    });
+
     return (
         <div className={ styles.VideoWrapper }>
-            <video playsInline autoPlay muted loop className={ styles.video }>
+            <video muted playsInline autoPlay loop className={ styles.video } ref={videoRef}>
                 <source src={ Video } type="video/mp4"/>
             </video>
 

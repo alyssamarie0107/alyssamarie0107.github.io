15bc62ecccbab5e56717b97cb2693f1d77d7ae3b
alyssa rodriguez <amrrodriguez@ucdavis.edu>
2023-01-13 10:18:58 -0800

    test handleSuspend

--- src/components/video-wrapper/VideoWrapper.js
+++ src/components/video-wrapper/VideoWrapper.js
@@ -11,9 +11,18 @@ const VideoWrapper = (props) => {
         videoRef.current.defaultMuted = true;
     });
 
+    // video autoplays on mobile, however doesn't if it is on 'low power mode'
+    // to detect if on low power mode, see if it has been suspended 
+    // handles when video is suspended do to mobile device being on 'low power mode'
+    const handleSuspend = (e) => {
+        e.preventDefault();
+
+        videoRef.current.play();
+    }
+
     return (
         <div className={ styles.VideoWrapper }>
-            <video muted playsInline autoPlay loop className={ styles.video } ref={videoRef}>
+            <video muted playsInline autoPlay loop className={ styles.video } ref={videoRef} onSuspend = { handleSuspend }>
                 <source src={ Video } type="video/mp4"/>
             </video>
 

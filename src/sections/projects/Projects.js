8b75cd85ef5abc92f0e62d2175a4ed3612202175
alyssa rodriguez <amrrodriguez@ucdavis.edu>
2023-03-03 17:00:42 -0800

    update project descriptions to be more detailed (still need to do interactive media design portfolio)

--- src/sections/projects/Projects.js
+++ src/sections/projects/Projects.js
@@ -73,7 +73,13 @@ const Projects = () => {
                                 alt="Pomocon"
                                 imgStyling="webImg"
                                 projectTitle="Pomocon"
-                                projectDescription="Project submission for my first hackathon, HackDavis 2021. The theme of this year was for the betterment of social good. My partner and I created a pomodoro timer to help users differentiate time to work from time to relax in the midst of the COVID-19 pandemic."
+                                projectDescription="Pomocon is a web application for the HackDavis 2021 hackathon. The theme of the hackathon was 'for the betterment of social good' and Pomocon was designed to help users manage their time more effectively during the Covid-19 pandemic.
+
+                                The main purpose of Pomocon is to help users differentiate their work time from their relaxation time. The application is built using HTML, CSS, JavaScript, and Bootstrap. The pomodoro timer is a popular time management technique that involves breaking work down into 25-minute intervals, followed by short breaks. Pomocon takes this technique and makes it easy for users to follow.
+
+                                The application is user-friendly and responsive, with a clean and modern interface. Users can set the timer for work intervals and breaks, and the application will alert them when it's time to switch. Additionally, Pomocon also provides users with valuable statistics about their productivity. Users can view how many pomodoros they have completed, how much time they spent on work, how long they took breaks, and their work efficiency percentage. These statistics help users to identify areas where they can improve and optimize their time management strategies.
+
+                                Overall, Pomocon is a powerful tool for anyone looking to improve their productivity and manage their time more effectively. With its simple interface, powerful features, and insightful statistics, it's an excellent addition to any time management toolkit."
                                 projectViewBtns={
                                     <div className={ styles.projectViewBtns }>
                                         <ViewBtn href="https://alyssamarie0107.github.io/pomocon/">View Website</ViewBtn>
@@ -93,7 +99,11 @@ const Projects = () => {
                                 alt="Lost & Found"
                                 imgStyling="webImg"
                                 projectTitle="UCD Lost & Found"
-                                projectDescription="A full-stack web app to act as a Lost & Found page for UC Davis. NOTE: This web app can only be fully accessed by users with a valid ucdavis.edu email. Users who do not have a valid ucdavis.edu email can only view the login page."
+                                projectDescription="UCD Lost & Found is a full-stack web application designed to serve as a lost and found page for the UC Davis community. The goal of the project is to provide a platform for students, faculty, and staff to post lost and found items, search for lost items, and search through found items. The project was built using HTML, CSS, and JavaScript for the front-end, and Node.js, Express.js, SQL, and SQLite3 for the back-end.
+                                
+                                One of the key features of the application is its secure login system. Only users with a valid UC Davis email address can access the application, ensuring that only members of the UC Davis community can post or search for lost and found items.
+
+                                The application allows users to search for lost and found items by title, category, and location. Users can also view a map of the campus to see where lost and found items have been reported. When a user finds a lost item, they can create a post describing the item and its location. When a user loses an item, they can create a post describing the item, its location when last seen, and other relevant details. Overall, UCD Lost & Found is a powerful tool for the UC Davis community. With its secure login system and powerful search and filter capabilities, it's an excellent resource for anyone who has lost or found an item on campus."
                                 projectViewBtns={
                                     <div className={ styles.projectViewBtns }>
                                         <ViewBtn href="https://metacoders-ucd-lf.glitch.me/">View Website</ViewBtn>
@@ -115,7 +125,13 @@ const Projects = () => {
                                 alt="Postcard Generator"
                                 imgStyling="webImg"
                                 projectTitle="Postcard Generator"
-                                projectDescription="A web app that enables users to create a postcard sharing a moment of their day to their friends, family, and loved ones. NOTE: This app takes some time to load due to it being viewed on glitch.com."
+                                projectDescription="Postcard Generator is a web application that enables users to create and share personalized postcards with their friends, family, and loved ones. The project was built using HTML, CSS, and JavaScript, and is hosted on glitch.com (NOTE: when visiting, the application will take a moment to load).
+                                
+                                One of the key features of the Postcard Generator is the ability to customize the design of the postcard that are truly unique and reflect their personal style. Users can upload a photo, which is then added to the postcard as an element and users can choose a background color for their postcard, select a font for their message, and add a custom message to their postcard.
+                                
+                                Another important feature of the application is its sharing functionality. Once a user has created their postcard, they can generate a link to share it with others. The generated link can be shared through email, social media, or any other preferred communication method. This makes it easy for users to share their postcards with friends and family all around the world.
+
+                                In summary, Postcard Generator is a user-friendly web application that allows users to create and share personalized postcards with their loved ones. With its customization options and easy sharing functionality, it's a fun and convenient way to stay connected with friends and family."
                                 projectViewBtns={
                                     <div className={ styles.projectViewBtns }>
                                         <ViewBtn href="https://post-tranquil-creator.glitch.me/">View Website</ViewBtn>
@@ -134,7 +150,11 @@ const Projects = () => {
                                 alt="Museum Exhibit"
                                 imgStyling="webImg"
                                 projectTitle="Museum Exhibit"
-                                projectDescription="The campus exhibit, Manetti Shrum, was closed as part of the campus shut-down in 2020 due to the pandemic. This web app shows current art pieces from exhibit during that time so that people can still get an idea of what was inside of the museum. The header was a countdown to when it would possibly open again."
+                                projectDescription="Museum Exhibit is a web application that showcases the art pieces by Stephen Kaltenbach from the Manetti Shrem museum exhibit at the University of California, Davis. The project was created as a response to the museum's closure in 2020 due to the COVID-19 pandemic. It was built using HTML, CSS, JavaScript, and Bootstrap.
+
+                                The application features an easy-to-navigate interface that displays  some of Stephen Kaltenbach's art pieces from the exhibit using a slideshow component. Users can browse through the artwork and read about each piece. The header of the application features a countdown timer to when the exhibit would possibly open again, creating a sense of anticipation and excitement among users.
+                                
+                                Overall, Museum Exhibit is a powerful tool for showcasing art and keeping people engaged with the cultural offerings of the university during a challenging time."
                                 projectViewBtns={
                                     <div className={ styles.projectViewBtns }>
                                         <ViewBtn href="https://alyssamarie0107.github.io/museum-162-alyssa/">View Website</ViewBtn>
@@ -158,7 +178,11 @@ const Projects = () => {
                                 alt="iApply"
                                 imgStyling="mobileImg"
                                 projectTitle="iApply"
-                                projectDescription="Final project for ECS 189E: iOS App Development, which was taken in Winter 2021. My team and I created a job application management and preparation tool that eases the burden of tracking job applications so time can be better spent preparing for the opportunity itself."
+                                projectDescription="iApply is a comprehensive job application management and preparation tool designed to help users streamline the job application process. With iApply, users can easily create and manage applications by adding details such as the company name, application notes, due date, location, job requirements, and posting URL.
+                                
+                                One of the unique features of iApply is that it allows users to scan their resume and compare it to the job requirements of each company, using OCR and copy and pasting functionalities.
+                                
+                                The app also includes a To-Do list that can be used to keep track of upcoming deadlines, coding challenges, and interview dates, which is integrated with Apple Calendar for added convenience. With iApply, users can spend less time tracking applications and more time preparing for the opportunities themselves."
                                 projectViewBtns={
                                     <div className={ styles.projectViewBtns }>
                                         <ViewBtn href="https://github.com/ECS189E/project-w21-swifty-as-honey">View Code</ViewBtn>
@@ -174,7 +198,7 @@ const Projects = () => {
                                 alt="Card Bank"
                                 imgStyling="mobileImg"
                                 projectTitle="Card Bank"
-                                projectDescription="Mobile app also created for ECS 189E: iOS App Development. This app simply served as wallet. It takes in a phone number from the user and sends a verification code to the phone number via text message. The app then prompts the user to enter the code sent to verify identity. Once verified, the user's wallet is displayed visually by a table of accounts and information of each account."
+                                projectDescription="Card Bank is mobile application that was designed as a wallet with an added layer of security through two-factor authentication. The app prompts users to enter their phone number, sends a verification code to the number via text message, and prompts the user to enter the code to verify their identity. Once verified, the user's wallet is displayed visually in a table format with information on each account. The app allows users to manage multiple accounts and keep track of their balances."
                                 projectViewBtns={
                                     <div className={ styles.projectViewBtns }>
                                         <ViewBtn href="https://github.com/alyssamarie0107/card-bank-170">View Code</ViewBtn>
@@ -196,7 +220,9 @@ const Projects = () => {
                                 alt="User Thread"
                                 imgStyling="otherImg"
                                 projectTitle="User-level Thread Library"
-                                projectDescription="In this project for ECS 150: Operating Systems, my partner and I implemented a user-level thread library to support multithreading within the same process. As a prerequisite to the thread library we implemented a FIFO container (queue) which we used in subsequent steps. Moreover, we implemented a semaphore for synchronization, and implemented preemption to avoid unwanted uncooperative thread behavior while sharing resources."
+                                projectDescription="The User-Level Thread Library is a multithreading library implemented in C to enable running multiple threads within the same process. The library includes a FIFO queue, which was implemented as a prerequisite for the thread library. The queue is used to store the thread control blocks and to manage the order of thread execution. Additionally, we implemented a semaphore for synchronization and implemented preemption to avoid unwanted uncooperative thread behavior when sharing resources.
+
+                                This project allowed you to gain a better understanding of the concept of threads and asynchronous programming by implementing a basic user-level thread library for Linux. With this library, you can create multiple threads within a process and have them run concurrently, which can help improve the performance of your programs."
                                 projectViewBtns={
                                     <div className={ styles.projectViewBtns }>
                                         <ViewBtn href="https://github.com/alyssamarie0107/user-thread-lib150">View Code</ViewBtn>
@@ -213,7 +239,7 @@ const Projects = () => {
                                 alt="Hexapawn"
                                 projectTitle="Hexapawn"
                                 imgStyling="otherImg"
-                                projectDescription="Project built for ECS 170: Artificial Intelligence that extends the definition of hexapawn to include any similar game involving n white pawns, n black pawns, and a nxn board. The main task was to construct a Python function (and all supporting functions) which takes as input a representation of the state of a hexapawn game, an integer representing the size of the board, an indication as to which player is to move next, and an integer representing the number of moves to look ahead. This function returns as output the best next move that the designated player can make from that given board position."
+                                projectDescription="Hexapawn is a Python-based application that extends the classic Hexapawn game to any similar game that involves n white pawns, n black pawns, and an nxn board. The main objective of the project is to construct a Python function along with supporting functions that takes the current state of the board, the size of the board, the player to move next, and the number of moves to look ahead, and returns the best possible move that the player can make from the given board position by using the MiniMax search algorithm."
                                 projectViewBtns={
                                     <div className={ styles.projectViewBtns }>
                                         <ViewBtn href="https://github.com/alyssamarie0107/user-thread-lib150">View Code</ViewBtn>
